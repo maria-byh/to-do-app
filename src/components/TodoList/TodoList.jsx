e2028953c98643e31b0f01ead9e26b819877d5d1
maria2001-byh <59501786+maria2001-byh@users.noreply.github.com>
2022-02-22 19:57:58 +0100

    commit

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,16 +4,8 @@ import './TodoList.css';
 class TodoList extends Component  {
     render () {
         return (
-        <div className="TodoList">
+        <div className="TodoList container d-flex-column w-50 mt-5 pb-3 ">
             <h1>Todo List</h1>
-            <ul>
-            {this.props.items.map(item => (
-                <li key={item.id}>
-                <input type="checkbox" checked={item.done} onChange={() => this.props.onToggle(item.id)} />
-                <span className={item.done ? 'done' : ''}>{item.text}</span>
-                </li>
-            ))}
-            </ul>
         </div>
         );
     }
